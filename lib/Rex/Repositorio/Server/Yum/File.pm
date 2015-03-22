e24cd6172feb7968df6b3d1725e1c67a42355652
Jan <krimdomu@googlemail.com>
2015-03-22 21:45:22 +0100

    added proxy support for yum and apt. - #11

--- lib/Rex/Repositorio/Server/Yum/File.pm
+++ lib/Rex/Repositorio/Server/Yum/File.pm
@@ -9,6 +9,7 @@ package Rex::Repositorio::Server::Yum::File;
 use Mojo::Base 'Mojolicious::Controller';
 use File::Spec;
 use File::Path;
+use File::Basename qw'dirname';
 
 # VERSION
 
@@ -18,12 +19,27 @@ sub serve {
   my $file = $self->req->url;
   $self->app->log->debug("Serving: $file");
 
-  #  my $repo_dir = $self->app->get_repo_dir( repo => $self->repo->{name} );
+  my $full_repo_dir = $self->app->get_repo_dir( repo => $self->repo->{name} );
   my $repo_dir = File::Spec->rel2abs( $self->config->{RepositoryRoot} );
 
   $self->app->log->debug("Path: $repo_dir");
+  $self->app->log->debug("Full-Path: $full_repo_dir");
+
 
   my $serve_dir = File::Spec->catdir( $repo_dir, $file );
+
+  my $orig_file = $serve_dir;
+  $orig_file =~ s/\Q$full_repo_dir\E//;
+  $orig_file =~ s/\/$//;
+  $orig_file =~ s/^\///;
+  my $repo_url = $self->repo->{url};
+  $repo_url =~ s/\/$//;
+
+  $self->app->log->debug("Upstream-Repo-URL: $repo_url");
+  $self->app->log->debug("Upstream-File: $orig_file");
+
+  my $orig_file_url = $repo_url . "/" . $orig_file;
+  $self->app->log->debug("Original-File-URL: $orig_file_url" );
 
   if ( -d $serve_dir ) {
     my @entries;
@@ -47,8 +63,22 @@ sub serve {
     $self->render("file/serve");
   }
   else {
-    $self->app->log->debug("File-Download: $serve_dir");
-    return $self->render_file( filepath => $serve_dir );
+    if(-f $serve_dir) {
+      $self->app->log->debug("File-Download: $serve_dir");
+      return $self->render_file( filepath => $serve_dir );
+    }
+    else {
+      $self->app->log->debug("Need to get file from upstream: $orig_file_url");
+      return $self->proxy_to($orig_file_url, sub {
+        my ($c, $tx) = @_;
+        $c->app->log->debug("Got data from upstream...");
+        mkpath(dirname($serve_dir));
+        open my $fh, '>', $serve_dir or die($!);
+        binmode $fh;
+        print $fh $tx->res->body;
+        close $fh;
+      });
+    }
   }
 }
 
