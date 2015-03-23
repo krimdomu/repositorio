94ddae82ce9b52cd1f06265c3514022557620532
Jan <krimdomu@googlemail.com>
2015-03-23 21:03:46 +0100

    check etag of upstream server

---
+++ lib/Rex/Repositorio/Server/Helper/Proxy.pm
@@ -0,0 +1,52 @@
+package Rex::Repositorio::Server::Helper::Proxy;
+
+use base 'Mojolicious::Plugin';
+
+our $VERSION = '0.6';
+
+sub register {
+  my ( $self, $app ) = @_;
+
+  $app->helper(
+    proxy_to => sub {
+      my $c   = shift;
+      my $url = Mojo::URL->new(shift);
+      my $cb  = shift || sub { };
+
+      my %args = @_;
+      $url->query( $c->req->params ) if ( $args{with_query_params} );
+
+      if ( Mojo::IOLoop->is_running ) {
+        $c->render_later;
+        $c->ua->get(
+          $url,
+          sub {
+            my ( $self, $tx ) = @_;
+            _proxy_tx( $c, $tx, $cb );
+          }
+        );
+      }
+      else {
+        my $tx = $c->ua->get($url);
+        _proxy_tx( $c, $tx, $cb );
+      }
+    }
+  );
+}
+
+sub _proxy_tx {
+  my ( $self, $tx, $cb ) = @_;
+  if ( my $res = $tx->success ) {
+    $self->tx->res($res);
+    $self->rendered;
+    $cb->( $self, $tx );
+  }
+  else {
+    my $error = $tx->error;
+    $self->tx->res->headers->add( 'X-Remote-Status',
+      $error->{code} . ': ' . $error->{message} );
+    $self->render( status => 500, text => 'Failed to fetch data from backend' );
+  }
+}
+
+1;
--- lib/Rex/Repositorio/Server/Yum/File.pm
+++ lib/Rex/Repositorio/Server/Yum/File.pm
@@ -10,6 +10,7 @@ use Mojo::Base 'Mojolicious::Controller';
 use File::Spec;
 use File::Path;
 use File::Basename qw'dirname';
+use Mojo::UserAgent;
 
 # VERSION
 
@@ -25,7 +26,6 @@ sub serve {
   $self->app->log->debug("Path: $repo_dir");
   $self->app->log->debug("Full-Path: $full_repo_dir");
 
-
   my $serve_dir = File::Spec->catdir( $repo_dir, $file );
 
   my $orig_file = $serve_dir;
@@ -39,7 +39,7 @@ sub serve {
   $self->app->log->debug("Upstream-File: $orig_file");
 
   my $orig_file_url = $repo_url . "/" . $orig_file;
-  $self->app->log->debug("Original-File-URL: $orig_file_url" );
+  $self->app->log->debug("Original-File-URL: $orig_file_url");
 
   if ( -d $serve_dir ) {
     my @entries;
@@ -63,21 +63,55 @@ sub serve {
     $self->render("file/serve");
   }
   else {
-    if(-f $serve_dir) {
+
+    my $force_download = 0;
+    if ( -f "$serve_dir.etag" ) {
+
+      # there is an etag file, so the file was downloaded via proxy
+      # check upstream if file is out-of-date
+      my ($etag) = eval { local ( @ARGV, $/ ) = ("$serve_dir.etag"); <>; };
+      $self->app->log->debug(
+        "Making ETag request to upstream ($orig_file_url): ETag: $etag.");
+      my $tx = $self->ua->head( $orig_file_url,
+        { Accept => '*/*', 'If-None-Match' => $etag } );
+      if ( $tx->success ) {
+        my $upstream = $tx->res->headers->header('ETag');
+        $upstream =~ s/"//g;
+        if ( $upstream ne $etag ) {
+          $self->app->log->debug(
+            "Upstream ETag and local ETag does not match: $upstream != $etag");
+          $force_download = 1;
+        }
+        else {
+          $self->app->log->debug("Upstream ETag and local ETag are the same: $upstream == $etag");
+        }
+      }
+    }
+
+    if ( -f $serve_dir && $force_download == 0 ) {
       $self->app->log->debug("File-Download: $serve_dir");
       return $self->render_file( filepath => $serve_dir );
     }
     else {
       $self->app->log->debug("Need to get file from upstream: $orig_file_url");
-      return $self->proxy_to($orig_file_url, sub {
-        my ($c, $tx) = @_;
-        $c->app->log->debug("Got data from upstream...");
-        mkpath(dirname($serve_dir));
-        open my $fh, '>', $serve_dir or die($!);
-        binmode $fh;
-        print $fh $tx->res->body;
-        close $fh;
-      });
+      return $self->proxy_to(
+        $orig_file_url,
+        sub {
+          my ( $c, $tx ) = @_;
+          $c->app->log->debug("Got data from upstream...");
+          mkpath( dirname($serve_dir) );
+          open my $fh, '>', $serve_dir or die($!);
+          binmode $fh;
+          print $fh $tx->res->body;
+          close $fh;
+
+          my $etag = $tx->res->headers->header('ETag');
+          $etag =~ s/"//g;
+          open my $fh, '>', "$serve_dir.etag" or die($!);
+          print $fh $etag;
+          close $fh;
+        }
+      );
     }
   }
 }
